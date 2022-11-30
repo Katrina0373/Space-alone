8df86ce7f846a3983f47d3641fc79080697c2652
Katrina0373 <mocalovak6@gmail.com>
2022-11-30 22:28:10 +0300

    sprites for ui and new scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "level1";
+
+
+	public void LoadScene (string sceneToLoad)
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+	public void ExitPressed()
+    {
+		Application.Quit();
+		Debug.Log("Exit");
+    }
+}
