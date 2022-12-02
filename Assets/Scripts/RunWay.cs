2d2c812dfad9816a353fa8bfeab79fd82568ff0d
Katrina0373 <mocalovak6@gmail.com>
2022-12-02 23:47:44 +0300

    update level

---
+++ Assets/Scripts/RunWay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunWay : MonoBehaviour
+{
+    public Button on;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        if (on.IsPress)
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            collision.transform.parent = this.transform;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            collision.transform.parent = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "stop")
+        {
+            speed = 0;
+            GetComponent<Rigidbody2D>().isKinematic = false;
+            transform.position = new Vector2(transform.position.x, transform.position.y);
+        }
+    }
+}
